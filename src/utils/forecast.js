e5ca8874377aaceee6480020468daa40c2f85c0e
smeetthakkar <thakkersmeet@gmail.com>
2020-08-03 05:09:49 -0700

    Update forecast message output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,9 @@ const forecast = (latitude, longitude, callback) => {
             callback(body.error, undefined)
         } else {
             callback(undefined, 
-                body.current.weather_descriptions[0] + ' throughout the day' + 
-                ' .It is currently ' + body.current.temperature + ' degrees outside.'
+                body.current.weather_descriptions[0] + ' throughout the day.' + 
+                ' It is currently ' + body.current.temperature + ' degrees outside '
+                + ' but it feels like ' + body.current.feelslike + ' degrees.'
                 // desc: body.current.weather_descriptions[0],
                 // temperature: body.current.temperature
             )
